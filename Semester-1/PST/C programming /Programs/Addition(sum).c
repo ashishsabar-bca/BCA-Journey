91cd64b90d540fed9bf594e1f982b536514620f1
Ashish Sabar <ashishsabar002@gmail.com>
2026-08-05 21:58:54 +0530

    Create Addition(sum).c
    Output:
    4

---
+++ Semester-1/PST/C programming /Programs/Addition(sum).c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+ int a = 2, b = 2, sum = 0;
+ sum = a + b ;
+ printf("%d",sum);
+ return 0;
+}
+
+Or
+
+
+
+#include <stdio.h>
+
+int main()
+{
+ int a = 2;
+ int b = 2; 
+ int sum = 0;
+ sum = a + b ;
+ printf("%d",sum);
+ return 0;
+ }
