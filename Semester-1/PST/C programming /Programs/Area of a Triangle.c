94bddc2435d06dca4c703a48d0db6ee97a19e1f6
Ashish Sabar <ashishsabar002@gmail.com>
2026-08-20 22:02:57 +0530

    Area of a Triangle.c
                   Output
    ----------------------------------------------
    Enter the radius of the Triangle:2
    The area of the Triangle :12.560000

---
+++ Semester-1/PST/C programming /Programs/Area of a Triangle.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{ 
+ float pi = 3.14,r,area = 1;
+ printf("Enter the radius of the Triangle:");
+ scanf("%f",&r);
+ area = pi*r*r;
+ printf("The area of the Triangle :%f",area);
+ return 0;  
+}
