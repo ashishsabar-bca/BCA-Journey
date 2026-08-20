cea2e935a6cb01956bfcb7d5dedeaa56c2ecf2c7
Ashish Sabar <ashishsabar002@gmail.com>
2026-08-20 22:05:21 +0530

    Area of a Square.c
                 OUTPUT
    -------------------------------------------
    
    Enter the side of the square:2
    The area of the Square :4.000000

---
+++ Semester-1/PST/C programming /Programs/Area of a Square.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{ 
+ float side,area = 1;
+ printf("Enter the side of the square:");
+ scanf("%f",&side);
+ area = side*side;
+ printf("The area of the Square :%f",area);
+ return 0;  
+}
